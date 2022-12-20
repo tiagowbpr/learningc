459a2cc01de49e6e36eb439fcaeb778824fdaad0
Tiago Martins <tiagowbpr@gmail.com>
2022-12-20 20:17:36 -0300

    You can pre-increment array pointers too

--- learncp/learn.c
+++ learncp/learn.c
@@ -6,6 +6,9 @@ int func(unsigned char *chs)
     printf("%d\n", *chs++);
     printf("%d\n", *chs++);
     printf("%d\n", *chs++);
+    printf("%d\n", *++chs);
+    printf("%d\n", *++chs);
+    printf("%d\n", *++chs);
 }
 
 int main(int argc, char **argv)
@@ -15,6 +18,10 @@ int main(int argc, char **argv)
     chs[0] = 1;
     chs[1] = 2;
     chs[2] = 3;
+    chs[3] = 4;
+    chs[4] = 5;
+    chs[5] = 6;
+    chs[6] = 7;
     /* When creating an array you get a pointer to the first element */
     printf("%p\n", chs);
 
