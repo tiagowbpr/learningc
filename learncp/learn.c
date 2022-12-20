a0fb589a7d126e67fd754c877c6e09c407391684
Tiago Martins <tiagowbpr@gmail.com>
2022-12-20 17:35:42 -0300

    Converts cli arguments first char to hexadecimal

--- learncp/learn.c
+++ learncp/learn.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "../cache.h"
 
+static unsigned hexval(char c)
+{
+	if (c >= '0' && c <= '9')
+		return c - '0';
+	if (c >= 'a' && c <= 'f')
+		return c - 'a' + 10;
+	if (c >= 'A' && c <= 'F')
+		return c - 'A' + 10;
+	return ~0;
+}
 
-int main()
+int main(int argc, char **argv)
 {   
-    printf("%d\n", ~0);
-    printf("%0b\n", ~0);
+    int i;
+    for (i = 1; i < argc; i ++) {
+        printf("%s: %d\n", argv[i], hexval(argv[i][0]));
+    }
     return 0;
 }
