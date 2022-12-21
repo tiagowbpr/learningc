fee4e2eca921616ee8602ef4cd0d551d7c738851
Tiago Martins <tiagowbpr@gmail.com>
2022-12-21 08:10:43 -0300

    Prints the result of get_sha1_hex

--- read-tree.c
+++ read-tree.c
@@ -9,5 +9,8 @@ int main (int argc, char **argv)
         usage("read-tree <key>");
     if (get_sha1_hex(argv[1], sha1) < 0)
         usage("read-tree <key>");
+    for (int i = 0; i < 20; i++) {
+        printf("%d : %d\n", i + 1, sha1[i]);
+    }
     return 0;
 }
