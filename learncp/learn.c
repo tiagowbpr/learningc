6051277e2c12796dc0298742ce54a4183a39a00c
Tiago Martins <tiagowbpr@gmail.com>
2022-12-21 06:44:25 -0300

    Increments an array pointer and adds a letter

--- learncp/learn.c
+++ learncp/learn.c
@@ -3,29 +3,25 @@
 /* func receive a pointer to an array*/
 int func(unsigned char *chs)
 {
-    printf("%d\n", *chs++);
-    printf("%d\n", *chs++);
-    printf("%d\n", *chs++);
-    printf("%d\n", *++chs);
-    printf("%d\n", *++chs);
-    printf("%d\n", *++chs);
+    unsigned char start = 97;
+    int i;
+    for (i = 0; i < 20; i++) {
+        *chs++ = start;
+        start++;
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
 {   
     /* Trying to understand *sha1++ = val */
     unsigned char chs[20];
-    chs[0] = 1;
-    chs[1] = 2;
-    chs[2] = 3;
-    chs[3] = 4;
-    chs[4] = 5;
-    chs[5] = 6;
-    chs[6] = 7;
     /* When creating an array you get a pointer to the first element */
     printf("%p\n", chs);
 
     func(chs);
+
+    printf("%s\n", chs);
 
     return 0;
 }
